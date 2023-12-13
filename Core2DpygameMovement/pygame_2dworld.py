23570dd000a518ebcb6923ec49b95ae7f677885e
an0unym0us3 <100888141+an0unym0us3@users.noreply.github.com>
2023-12-13 20:15:58 +0900

    Rigged up, inaccurate, prototype minimap added

--- Core2DpygameMovement/pygame_2dworld.py
+++ Core2DpygameMovement/pygame_2dworld.py
@@ -4,17 +4,26 @@ pygame.init()
 window_w, window_h = 1280, 720
 screen = pygame.display.set_mode((window_w, window_h), pygame.RESIZABLE)
 
-screen_coefficient = 6
 true_bg = pygame.image.load('bg.png')
+
+screen_coefficient = 6
 bg = pygame.transform.scale(true_bg, (true_bg.get_width()*screen_coefficient, true_bg.get_height()*screen_coefficient))
 bgw, bgh = bg.get_width(), bg.get_height()
 
-spawn_x, spawn_y = 100*screen_coefficient, 0
+spawn_x, spawn_y = 100*screen_coefficient, 0*screen_coefficient
 player_pos = pygame.Vector2((screen.get_width() - bgw)//2 - spawn_x, (screen.get_height() - bgh)//2 - spawn_y)
 player_img = pygame.image.load('fplayer_wf.png')
 pgw, pgh = player_img.get_width(), player_img.get_height()
 display_center = pygame.Vector2((screen.get_width() - pgw)//2, (screen.get_height() - pgh)//2)
 speed = 10
+
+minimap_coefficient = 0.4
+mp_player_coefficient = 0.1
+mp = pygame.transform.scale(true_bg, (true_bg.get_width()*minimap_coefficient, true_bg.get_height()*minimap_coefficient))
+mp_w, mp_h = mp.get_width(), mp.get_height()
+mp_player = pygame.transform.scale(player_img, (player_img.get_width()*mp_player_coefficient, player_img.get_height()*mp_player_coefficient))
+mp_player_pos = pygame.Vector2(200, 100)
+mp_speed_coefficient = 0.07
 
 # coordinates
 # y=400->-2000 x=-3600->400
@@ -38,21 +47,30 @@ while game_run:
     screen.blit(player_img, (display_center.x, display_center.y))
     walking = False
 
+    pygame.draw.rect(screen, (164,116,73), [5, 5, mp_w+10, mp_h+10])
+    screen.blit(mp, (10, 10))
+    screen.blit(mp_player, (mp_player_pos.x, mp_player_pos.y))
+
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w] or keys[pygame.K_UP]:
         player_pos.y -= -speed
+        mp_player_pos.y -= speed * mp_speed_coefficient
         walking = True
         sprite_direction = 'b'
     if keys[pygame.K_s] or keys[pygame.K_DOWN]:
         player_pos.y += -speed
+        mp_player_pos.y += speed * mp_speed_coefficient
         walking = True
         sprite_direction = 'f'
     if keys[pygame.K_a] or keys[pygame.K_LEFT]:
         player_pos.x -= -speed
+        mp_player_pos.x -= speed * mp_speed_coefficient
         walking = True
         sprite_direction = 'l'
     if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
         player_pos.x += -speed
+        mp_player_pos.x += speed * mp_speed_coefficient
         walking = True
         sprite_direction = 'r'
 
