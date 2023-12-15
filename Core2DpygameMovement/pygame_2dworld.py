3878623371794bba02e02864ade554f0533d4777
an0unym0us3 <100888141+an0unym0us3@users.noreply.github.com>
2023-12-15 15:55:12 +0900

    trying to fix naming issues and redundancy

--- Core2DpygameMovement/pygame_2dworld.py
+++ Core2DpygameMovement/pygame_2dworld.py
@@ -82,3 +82,78 @@ while game_run:
     clock.tick(15)
 
 pygame.quit()
+
+
+'''
+import pygame as pg
+pg.init()
+
+window_w, window_h = 1024, 720
+display = pg.display.set_mode((window_w, window_h))
+window_c = (window_w//2, window_h//2)
+
+true_bg = pg.image.load('bg.png')
+
+bg_k = 6
+bg = pg.transform.scale(true_bg, (true_bg.get_width()*bg_k, true_bg.get_height()*bg_k))
+bg_w, bg_h = bg.get_width(), bg.get_height()
+
+spawn = (10, 60)
+map_pos = pg.Vector2(bg_w//2 * bg_k - spawn[0], bg_h//2 * bg_k - spawn[1])
+
+player_blit_pos = pg.Vector2(window_c[0], window_c[1])
+
+leg_cycle = ['r', '', 'l', '',]
+sprite_direction, this_leg = 'f', 0
+
+walking = False
+
+game_run = True
+clock = pg.time.Clock()
+while game_run:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            game_run = False
+
+    display.fill((0, 0, 0))
+    display.blit(bg, (player_pos.x, player_pos.y))
+
+    player_img = pg.image.load(f'{sprite_direction}player_w{leg_cycle[this_leg] if walking else ""}f.png')
+    display.blit(player_img, (display_center.x, display_center.y))
+    walking = False
+
+    pg.draw.rect(display, (164,116,73), [5, 5, mp_w+10, mp_h+10])
+    pg.blit(mp, (10, 10))
+    pg.blit(mp_player, (mp_player_pos.x, mp_player_pos.y))
+
+    text_surface = my_font.render(f"{pygame.mouse.get_pos()} {player_pos}", False, (0, 255, 0))
+    display.blit(text_surface, (0,0))
+
+    keys = pg.key.get_pressed()
+    if keys[pg.K_w] or keys[pg.K_UP]:
+        player_pos.y -= -speed
+        mp_player_pos.y -= speed * mp_speed_coefficient
+        walking = True
+        sprite_direction = 'b'
+    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+        player_pos.y += -speed
+        mp_player_pos.y += speed * mp_speed_coefficient
+        walking = True
+        sprite_direction = 'f'
+    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        player_pos.x -= -speed
+        mp_player_pos.x -= speed * mp_speed_coefficient
+        walking = True
+        sprite_direction = 'l'
+    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        player_pos.x += -speed
+        mp_player_pos.x += speed * mp_speed_coefficient
+        walking = True
+        sprite_direction = 'r'
+
+    this_leg = (this_leg + 1) % 3
+    pygame.display.flip()
+    clock.tick(15)
+
+pygame.quit()
+'''
